52f1a4a8183122f94793c48d30f803b8d23f02ed
Mohammad Erfani <113503524+erfani24@users.noreply.github.com>
2023-05-08 21:02:11 +0330

    Sell order Added

--- main.py
+++ main.py
@@ -10,21 +10,26 @@ import sched
 
 username = input('Username: ')
 password = input('Password: ')
-sarmaye = input('Mizan sarmaye baray kharid ra be Rial vared konid: ')
+order_type = int(input('Baraye Sefaresh kharid adad 1 va baraye foroosh adad 2 ra vared konid:  '))
+if order_type == 1:
+    sarmaye = input('Mizan sarmaye baray kharid ra be Rial vared konid: ')
+elif order_type == 2:
+    volume = int(input('Tedad sahmi ke ghasd forosh darid ra vared konid: '))
 start_time = input('Saati ke mikhahid ersal sefaresh aghaz shava vared konid(Format: HH:MM:SS): ')
 delay = input('Fasele bein orderha ra be mili sanie vared konid(Hadeaghal 300): ')
 order_quantity = input('Tedad orderha: ')
 order_quantity = int(order_quantity)
 delay = int(delay)/1000
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='D:\Projects\chromedriver.exe')
 driver.maximize_window()
 driver.get('https://www.nahayatnegar.com/online')
 
 # login
 driver.find_element(By.NAME, 'userEmail').send_keys(username)
 driver.find_element(By.NAME, 'userPassword').send_keys(password)
-time.sleep(10)
+driver.execute_script("alert('کد امنیتی را وارد کنید!');")
+time.sleep(20)
 try:
     driver.find_element(By.NAME, 'submit').click()
 except:
@@ -38,38 +43,47 @@ time.sleep(1)
 try:
     driver.find_element(By.XPATH,"//*[contains(text(), 'سرخطی')]").click()
 except:
-    print('Dideban peyda nashod!')
+    driver.execute_script("alert('دیده بان سرخطی پیدا نشد!');")
     
 time.sleep(10)
-
-WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[3]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/table/tbody/tr[1]"))).click()
-time.sleep(2)
-driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/table/tbody/tr[1]')
-time.sleep(2)
+try:
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[3]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/table/tbody/tr[1]"))).click()
+    time.sleep(2)
+except:
+    driver.execute_script("alert('روی سهم مورد نظر کلیک کنید!');")
+    pass
 
 # Enter to Info Tab
 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[2]/div[1]"))).click()
 
-# time.sleep(1)
-price = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[2]/div[4]/div[2]/span[3]"))).text.replace(',', '')
-
-# Enter to Buy Tab
-WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[2]/div[2]"))).click()
-
-time.sleep(1)
-
-# fill thet order fields
-driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[1]/div[1]/div[2]/div/input').send_keys(price)
-volume = (int(sarmaye) / int(price)) - 10
-driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[1]/div[3]/div[2]/div[1]/input').send_keys(int(volume))
-time.sleep(5)
-
-driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[3]/input').click()
-buy_button = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[4]/div[1]/button')
-
+if order_type == 1:
+    price = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[2]/div[4]/div[2]/span[3]"))).text.replace(',', '')
+    # Enter to Buy Tab
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[2]/div[2]"))).click()
+    time.sleep(1)
+    # fill the order fields
+    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[1]/div[1]/div[2]/div/input').send_keys(price)
+    volume = (int(sarmaye) / int(price)) - 10
+    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[1]/div[3]/div[2]/div[1]/input').send_keys(int(volume))
+    time.sleep(5)
+    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[3]/input').click()
+    send_order_button = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[4]/div[1]/button')
+    
+elif order_type == 2:
+    price = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[2]/div[4]/div[2]/span[1]"))).text.replace(',', '')
+    # Enter to sell Tab
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]"))).click()
+    time.sleep(1)
+    # fill the order fields
+    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[1]/div[1]/div[2]/div/input').send_keys(price)
+    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[1]/div[3]/div[2]/div[1]/input').send_keys(int(volume))
+    time.sleep(5)
+    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[3]/input').click()
+    send_order_button = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[3]/div[1]/div[2]/div[1]/div[1]/div/div/form/div[4]/div[1]/button')
+    
 def send_order():
     for i in range(order_quantity):
-        buy_button.click()
+        send_order_button.click()
         time.sleep(delay)
 
 
@@ -88,5 +102,5 @@ scheduler.enter(time_diff, 1, send_order, ())
 
 # start the scheduler
 scheduler.run()
-
+driver.execute_script("alert('ارسال سفارش با موفقیت انجام شد!');")
 time.sleep(20)
